db6c576ae6cd77a512c59d6cc9e32e725184bc77
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-09-15 15:54:40 +0300

    pr13 iter1

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6321c6e6e5303b2fabcab88e',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
