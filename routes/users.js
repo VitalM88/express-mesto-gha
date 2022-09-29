8854a698cb6df37fb34ca018a84e5aba7a2820ea
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-09-29 16:08:46 +0300

    pr14 iter1

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { validateUrl } = require('../utils/patternUrl');
+//const { validateUrl } = require('../utils/patternUrl');
 
 const {
   getUsers,
