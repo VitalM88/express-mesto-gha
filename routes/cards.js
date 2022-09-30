21fb9925ef2786373241b41ff200451bcd7cf2aa
Vitalii Matveenkov <vitalii_matveenkov@mail.ru>
2022-09-30 12:13:33 +0300

    pr14 iter1

--- routes/cards.js
+++ routes/cards.js
@@ -9,13 +9,13 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     link: Joi.string().min(2),
   }),
 }), createCard);
+router.get('/', getCards);
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
